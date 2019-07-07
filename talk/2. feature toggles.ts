2f56b805ed9026778565dc990a7e2375d44b84ef
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-07-07 16:37:02 +0800

    Adding examples 1 & 2

--- talk/2. feature toggles.ts
+++ talk/2. feature toggles.ts
@@ -1,9 +1,12 @@
 import { setFeatures } from '../src/toggles'
 
-const features = setFeatures('feature1', 'feature2')
+// const features = setFeatures('feature1', 'feature2')
+const features = setFeatures({
+    feature1: true,
+    feature2: { enabled: false, userCanOverride: true },
+})
 
 features.activate('feature1')
-features.deactivate('feature2')
 
 features.isActive('feature1') //?
 features.isActive('feature2') //?
