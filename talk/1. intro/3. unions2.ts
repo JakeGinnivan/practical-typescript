41471b3fe70a5de1ee2b7bcf154744d90dc65da7
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-07-09 22:18:23 +0800

    Updated union demos

--- talk/1. intro/3. unions2.ts
+++ talk/1. intro/3. unions2.ts
@@ -1,17 +1,23 @@
-export const attendees: Array<Speaker | Delegate> = []
+export const attendees: Array<Speaker | Delegate | Organiser> = []
 
 attendees.push({ name: 'Jake', talk: 'Getting the most out of TypeScript' })
 attendees.push({ name: 'Jane', ticket: 'day1' })
+attendees.push({ name: 'Jeff' })
 
 function isSpeaker(attendee): attendee is Speaker {
     return 'talk' in attendee
+}
+function isDelegate(attendee): attendee is Delegate {
+    return 'ticket' in attendee
 }
 
 const list = attendees.map(attendee => {
     if (isSpeaker(attendee)) {
         return `Speaker ${attendee.name} speaking about ${attendee.talk}`
-    } else {
+    } else if (isDelegate(attendee)) {
         return `${attendee.ticket} delegate: ${attendee.name}`
+    } else {
+        return `${attendee.name} is one of our awesome organisers`
     }
 })
 
@@ -26,3 +32,5 @@ interface Speaker extends Person {
 interface Delegate extends Person {
     ticket: 'day1' | 'day2' | 'all-days'
 }
+
+interface Organiser extends Person {}
--- talk/1. intro/done/3. unions2.done.ts
+++ talk/1. intro/done/3. unions2.done.ts
@@ -1,14 +1,19 @@
-export const attendees: Array<Speaker | Delegate> = []
+export const attendees: Array<Speaker | Delegate | Organiser> = []
 
 attendees.push({ kind: 'speaker', name: 'Jake', talk: 'Getting the most out of TypeScript' })
 attendees.push({ kind: 'delegate', name: 'Jane', ticket: 'day1' })
+attendees.push({ kind: 'organiser', name: 'Jeff' })
 
 const list = attendees.map(attendee => {
     if (attendee.kind === 'speaker') {
         return `Speaker ${attendee.name} speaking about ${attendee.talk}`
     }
 
-    return `${attendee.ticket} delegate: ${attendee.name}`
+    if (attendee.kind === 'delegate') {
+        return `${attendee.ticket} delegate: ${attendee.name}`
+    }
+
+    return `${attendee.name} is one of our awesome organisers`
 })
 
 list
@@ -23,4 +28,8 @@ interface Speaker extends Person {
 interface Delegate extends Person {
     kind: 'delegate'
     ticket: 'day1' | 'day2' | 'all-days'
+}
+
+interface Organiser extends Person {
+    kind: 'organiser'
 }
